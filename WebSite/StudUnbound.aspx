7f88f7ee237e3987c421e5e2da5e646a24ce501d
shahpiyushn <shahpiyushn@yahoo.co.in>
2017-05-16 08:29:36 +0530

    Add files via upload

--- WebSite/StudUnbound.aspx
+++ WebSite/StudUnbound.aspx
@@ -17,11 +17,21 @@
 &nbsp;<asp:TextBox ID="txtPer" runat="server"></asp:TextBox>
     </p>
     <p>
-        <asp:Button ID="BtnInsert" runat="server" Text="Insert" />
-        <asp:Button ID="BtnUpdate" runat="server" Text="Update" />
-        <asp:Button ID="BtnDelete" runat="server" Text="Delete" />
-        <asp:Button ID="BtnSearch1" runat="server" Text="Search1" />
-        <asp:Button ID="BtnSearch2" runat="server" Text="Search2" />
+        <asp:Button ID="BtnInsert" runat="server" Text="Insert" 
+            onclick="BtnInsert_Click" />
+        <asp:Button ID="BtnUpdate" runat="server" Text="Update" 
+            onclick="BtnUpdate_Click" />
+        <asp:Button ID="BtnDelete" runat="server" Text="Delete" 
+            onclick="BtnDelete_Click" />
+        <asp:Button ID="BtnSearch1" runat="server" Text="Search1" 
+            onclick="BtnSearch1_Click" />
+        <asp:Button ID="BtnSearch2" runat="server" Text="Search2" 
+            onclick="BtnSearch2_Click" />
+    &nbsp;<asp:Button ID="BtnRefresh" runat="server" onclick="BtnRefresh_Click" 
+            Text="Refresh" />
+    </p>
+    <p>
+        <asp:Label ID="lblMsg" runat="server" EnableViewState="False"></asp:Label>
     </p>
     <p>
         <asp:GridView ID="GridView1" runat="server">
--- WebSite/StudUnbound.aspx.cs
+++ WebSite/StudUnbound.aspx.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections;
 using System.Configuration;
-using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.Security;
@@ -10,6 +9,7 @@ using System.Web.UI.HtmlControls;
 using System.Web.UI.WebControls;
 using System.Web.UI.WebControls.WebParts;
 using System.Xml.Linq;
+using System.Data;
 using System.Data.OleDb;
 
 public partial class StudUnbound : System.Web.UI.Page
@@ -26,6 +26,11 @@ public partial class StudUnbound : System.Web.UI.Page
         con = new OleDbConnection(scon);
         con.Open();
 
+        BindGrid();
+    }
+
+    void BindGrid()
+    {
         ssql = "select * from stud";
         da = new OleDbDataAdapter(ssql, con);
         ds = new DataSet();
@@ -33,5 +38,81 @@ public partial class StudUnbound : System.Web.UI.Page
 
         GridView1.DataSource = ds.Tables[0];
         GridView1.DataBind();
+    }
+    void clearTextbox()
+    {
+        txtRn.Text = "";
+        txtName.Text = "";
+        txtPer.Text = "";
+    }
+    protected void BtnInsert_Click(object sender, EventArgs e)
+    {
+        //ssql = "insert into stud values(9,'chiranjit',65)";
+        ssql = "insert into stud values("+ txtRn.Text +",'"+ txtName.Text +"',"+ txtPer.Text +")";
+
+        com = new OleDbCommand(ssql, con);
+        com.ExecuteNonQuery();
+
+        lblMsg.Text = "Insert success...";
+
+        BindGrid();
+        clearTextbox();
+    }
+    protected void BtnUpdate_Click(object sender, EventArgs e)
+    {
+        //ssql = "update stud set name='bbb',per=65 where rn=8";
+        ssql = "update stud set name='" + txtName.Text + "',per=" + txtPer.Text + "   where rn=" + txtRn.Text + "";
+
+        com = new OleDbCommand(ssql, con);
+        com.ExecuteNonQuery();
+
+        lblMsg.Text = "Update success...";
+
+        BindGrid();
+        clearTextbox();
+    }
+    protected void BtnDelete_Click(object sender, EventArgs e)
+    {
+        //ssql = "delete from stud where rn=8";
+        ssql = "delete from stud where rn=" + txtRn.Text + "";
+
+        com = new OleDbCommand(ssql, con);
+        com.ExecuteNonQuery();
+
+        lblMsg.Text = "Delete success...";
+
+        BindGrid();
+        clearTextbox();
+    }
+    protected void BtnSearch1_Click(object sender, EventArgs e)
+    {
+        ssql = "select * from stud where rn="+ txtRn.Text +"";
+        da = new OleDbDataAdapter(ssql, con);
+        ds = new DataSet();
+        da.Fill(ds);
+
+        GridView1.DataSource = ds.Tables[0];
+        GridView1.DataBind();
+    }
+    protected void BtnRefresh_Click(object sender, EventArgs e)
+    {
+        BindGrid();
+    }
+    protected void BtnSearch2_Click(object sender, EventArgs e)
+    {
+        ssql = "select * from stud where rn=" + txtRn.Text + "";
+        da = new OleDbDataAdapter(ssql, con);
+        ds = new DataSet();
+        da.Fill(ds);
+
+        if (ds.Tables[0].Rows.Count > 0)
+        {
+            txtName.Text = ds.Tables[0].Rows[0][1].ToString();
+            txtPer.Text = ds.Tables[0].Rows[0]["per"].ToString();
+        }
+        else
+        {
+            lblMsg.Text = "no record found...";
+        }
     }
 }
